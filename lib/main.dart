198ae9eaede46090736be9d2843b0d271543f7e9
fahiem152 <amfahiem010502@gmail.com>
2024-01-02 16:43:25 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:chat_app/pages/authenticate_page.dart';
 import 'package:chat_app/pages/login_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
+
+GoogleSignIn _googleSignIn = GoogleSignIn(
+  scopes: [
+    'email',
+    // 'https://www.googleapis.com/auth/contacts.readonly',
+  ],
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,6 +27,71 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: LoginPage());
+        home: SigninWithGoogle());
+  }
+}
+
+class SigninWithGoogle extends StatefulWidget {
+  const SigninWithGoogle({Key? key}) : super(key: key);
+
+  @override
+  State<SigninWithGoogle> createState() => _SigninWithGoogleState();
+}
+
+class _SigninWithGoogleState extends State<SigninWithGoogle> {
+  GoogleSignInAccount? _currentUser;
+  Future<void> _handleSignIn() async {
+    try {
+      await _googleSignIn.signIn();
+    } catch (error) {
+      print("error: $error");
+    }
+  }
+
+  @override
+  void initState() {
+    _googleSignIn.onCurrentUserChanged.listen((event) {
+      setState(() {
+        _currentUser = event;
+        print("user: $_currentUser");
+      });
+    });
+    _googleSignIn.signInSilently();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: _currentUser == null ? Text("Login") : Text("Dashboard"),
+        actions: const [],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              if (_currentUser == null)
+                Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueGrey,
+                    ),
+                    onPressed: () => _handleSignIn(),
+                    child: const Text("Login with Google"),
+                  ),
+                ),
+              if (_currentUser != null)
+                ListTile(
+                  leading: GoogleUserCircleAvatar(identity: _currentUser!),
+                  title: Text(_currentUser!.displayName ?? 'kosong'),
+                  subtitle: Text(_currentUser!.email ?? "Email Kosong"),
+                )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
